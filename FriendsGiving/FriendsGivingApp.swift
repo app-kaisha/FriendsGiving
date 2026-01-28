b287102b0034945385ee7b76066fd1ea0dd061ff
app-kaisha <app.kaihatsusha@gmail.com>
2026-01-28 21:08:14 +0000

    Initial Commit

---
+++ FriendsGiving/FriendsGivingApp.swift
@@ -0,0 +1,18 @@
+//
+//  FriendsGivingApp.swift
+//  FriendsGiving
+//
+//  Created by app-kaihatsusha on 28/01/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct FriendsGivingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
